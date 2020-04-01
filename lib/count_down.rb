3186b9991b2dc5cc53cf4b29f508dd2d60ef2c79
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-01 02:04:59 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,10 +1,10 @@
 # Write your code here
-#count = 10 
-#while count >= 1 do
-  #puts count
-  #count -= 1 
-#end
-#puts "Happy New Year!"
+count = 10 
+while count >= 1 do
+  puts count
+  count -= 1 
+end
+puts "Happy New Year!"
 
 count = 0 
 while count > 5 do 
